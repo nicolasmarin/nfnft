13d34a36e2f3f4b6b90066598a9ff575e62ad83d
agsola <git@agsola.com>
2023-07-08 22:23:53 +0200

    mute spam

--- frontend/src/templates/Main.tsx
+++ frontend/src/templates/Main.tsx
@@ -39,11 +39,6 @@ const Main = (props: IMainProps) => (
                 <Image src="/logo.svg" width={32} height={32} className="mr-2 h-8" alt={`${AppConfig.title} Logo`} />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap">{AppConfig.title}</span>
               </Link>
-              <Link target="_blank" rel="noreferrer" className="flex gap-2 bg-white dark:bg-gray-200 justify-center px-4 py-2 border-2 rounded-md my-6 text-xs font-light shadow-lg cursor-pointer no-underline" href="https://nfts2me.com">
-                <div className="mt-1">Created by</div>
-                <Image className="h-6" width={24} height={24} src="https://nfts2me.com/assets/images/logo.svg" alt="NFTs2Me logo" />
-                <div className="mt-1 font-bold">NFTs2Me Team</div>
-              </Link>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6">
               <div>
@@ -74,7 +69,7 @@ const Main = (props: IMainProps) => (
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-700 sm:text-center">© 2023 <Link href="/">{AppConfig.title}</Link>. All Rights Reserved. </span>
             <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-              <a href="https://twitter.com/nfts2me" target="_blank" className="text-gray-700 hover:text-black dark:hover:text-white">
+              <a href="https://twitter.com/agsola" target="_blank" className="text-gray-700 hover:text-black dark:hover:text-white">
                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
                 </svg>
