5ede14e081cb730378c8a9f6371c6a4e6730ac38
Nicolas Marin <nicolas@archivados.com>
2023-07-08 12:34:02 +0200

    Added variables of the creation form

--- frontend/src/pages/app/create.tsx
+++ frontend/src/pages/app/create.tsx
@@ -8,8 +8,20 @@ import Link from 'next/link';
 import { useNetwork } from 'wagmi';
 import UploadImage from '@/components/UploadImage';
 import Tooltip from '@/components/Tooltip';
+import { ChangeEvent, useState } from 'react';
 
 const Index = () => {
+  const [projectName, setProjectName] = useState<string>("");
+  const [projectSymbol, setProjectSymbol] = useState<string>("");
+  const [projectDescription, setProjectDescription] = useState<string>("");
+  const [projectFee, setProjectFee] = useState<number>(0.1);
+  const [projectSize, setProjectSize] = useState<number>(100);
+  const [projectSettingPrimarySale, setProjectSettingPrimarySale] = useState<number>(0);
+  const [projectSettingSecondarySale, setProjectSettingSecondarySale] = useState<number>(7.5);
+  const [projectSettingStakingRewards, setProjectSettingStakingRewards] = useState<number>(0);
+  const [projectSettingPenalty, setProjectSettingPenalty] = useState<number>(5);
+  const [projectSettingDaysPenalty, setProjectSettingDaysPenalty] = useState<number>(365);
+  const [artworkURL, setArtworkURL] = useState<string>("");
   const { chain: activeChain } = useNetwork();
 
   return (
@@ -140,14 +152,26 @@ const Index = () => {
                       Project Name
                       <Tooltip id="contractNameTip" message="Enter the name of your project" />
                     </label>
-                    <input type="text" name="contract-name" placeholder="Project Name" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:text-gray-500 " value="" />
+                    <input 
+                      type="text" name="contract-name" placeholder="Project Name" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:text-gray-500"
+                      value={projectName}
+                      onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                        setProjectName(e?.target?.value);
+                      }}
+                    />
                   </div>
                   <div className="field mt-4 w-full md:w-1/3 md:pl-4">
                     <label className="font-bold">
                       Token Symbol
                       <Tooltip id="tokenSymbolTip" message="Enter the name of your symbol" />
                     </label>
-                    <input type="text" name="token-symbol" placeholder="Token Symbol" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:text-gray-500 " value="" />
+                    <input 
+                    type="text" name="token-symbol" placeholder="Token Symbol" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:text-gray-500"
+                    value={projectSymbol}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      setProjectSymbol(e?.target?.value);
+                    }}
+                  />
                   </div>
                 </div>
                 <div className="field mt-4">
@@ -156,7 +180,12 @@ const Index = () => {
                     <Tooltip id="descriptionTip" message="Enter the description of your project" />
                   </label>
                   
-                  <textarea name="project" placeholder="This description will be visible to the public on platforms such as marketplaces and third-party applications, among others." className="w-full p-2 mt-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400"></textarea>
+                  <textarea name="project" placeholder="This description will be visible to the public on platforms such as marketplaces and third-party applications, among others." className="w-full p-2 mt-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400"
+                    value={projectDescription}
+                    onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+                      setProjectDescription(e?.target?.value);
+                    }}
+                  />
                 </div>
               </div>
               <div>
@@ -173,7 +202,13 @@ const Index = () => {
                             <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM11 16H13V18H11V16ZM12.61 6.04C10.55 5.74 8.73 7.01 8.18 8.83C8 9.41 8.44 10 9.05 10H9.25C9.66 10 9.99 9.71 10.13 9.33C10.45 8.44 11.4 7.83 12.43 8.05C13.38 8.25 14.08 9.18 14 10.15C13.9 11.49 12.38 11.78 11.55 13.03C11.55 13.04 11.54 13.04 11.54 13.05C11.53 13.07 11.52 13.08 11.51 13.1C11.42 13.25 11.33 13.42 11.26 13.6C11.25 13.63 11.23 13.65 11.22 13.68C11.21 13.7 11.21 13.72 11.2 13.75C11.08 14.09 11 14.5 11 15H13C13 14.58 13.11 14.23 13.28 13.93C13.3 13.9 13.31 13.87 13.33 13.84C13.41 13.7 13.51 13.57 13.61 13.45C13.62 13.44 13.63 13.42 13.64 13.41C13.74 13.29 13.85 13.18 13.97 13.07C14.93 12.16 16.23 11.42 15.96 9.51C15.72 7.77 14.35 6.3 12.61 6.04Z"></path>
                           </svg>
                         </div>
-                        <input type="number" step="any" name="minting-fee" placeholder="0.1" min="0" max="999999999999999999999999999999" className="w-full p-2 pl-9 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500     disabled:text-gray-500" value="0.1" />
+                        <input 
+                          type="number" step="any" name="minting-fee" placeholder="0.1" min="0" max="999999999999999999999999999999" className="w-full p-2 pl-9 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500     disabled:text-gray-500"
+                          value={projectFee}
+                          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                            setProjectFee(parseFloat(e?.target?.value));
+                          }}
+                        />
                         <div className="absolute min-w-[100px] h-8 mt-[6.5px] justify-center right-1.5 inset-y-0 flex items-center px-2 rounded-3xl opacity-8 text-black bg-slate-200
                         cursor-pointer">
                           <div className="mr-2">
@@ -190,10 +225,16 @@ const Index = () => {
                   <div className="text-lg font-bold text-black w-full md:w-1/3">
                     <div className="field mt-4">
                       <label>
-                        Edition Size
+                        Project Size
                         <Tooltip id="sizeTip" message="Enter the total supply of your project" />
                       </label>
-                      <input type="number" min="1" max="50000" name="total-supply" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500       disabled:text-gray-500" value="100" />
+                      <input
+                        type="number" min="1" name="total-supply" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500       disabled:text-gray-500"
+                        value={projectSize}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                          setProjectSize(parseInt(e?.target?.value));
+                        }}
+                      />
                     </div>
                   </div>
                 </div>
@@ -208,7 +249,13 @@ const Index = () => {
                     <div className="text-lg font-bold text-black">
                       <div className="field">
                         <div className="relative">
-                          <input type="number" step="any" max="10" min="0" name="percentage" placeholder="5%" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:text-gray-500" value="0" />
+                          <input
+                            type="number" step="any" max="10" min="0" name="percentage" placeholder="5%" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:text-gray-500"
+                            value={projectSettingPrimarySale}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                              setProjectSettingPrimarySale(parseFloat(e?.target?.value));
+                            }}
+                          />
                           <div className="absolute right-5 top-3.5 text-xs md:top-3 md:text-sm">
                             <label>Mint Percentage (max 10%)</label>
                           </div>
@@ -224,7 +271,10 @@ const Index = () => {
                     <div className="text-lg font-bold text-black">
                       <div className="field">
                         <div className="relative">
-                          <input type="number" step="any" max="10" min="0" name="percentage" placeholder="5%" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500    disabled:text-gray-500" value="7.5" />
+                          <input
+                            type="number" step="any" max="10" min="0" name="percentage" placeholder="5%" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500    disabled:text-gray-500"
+                            value="7.5"
+                          />
                           <div className="absolute right-5 top-3.5 text-xs md:top-3 md:text-sm">
                             <label>Royalties Percentage (max 10%)</label>
                           </div>
@@ -240,7 +290,13 @@ const Index = () => {
                     <div className="text-lg font-bold text-black">
                       <div className="field">
                         <div className="relative">
-                          <input type="number" step="any" max="10" min="0" name="percentage" placeholder="5%" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500    disabled:text-gray-500" value="0" />
+                          <input
+                            type="number" step="any" max="10" min="0" name="percentage" placeholder="5%" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500    disabled:text-gray-500"
+                            value={projectSettingStakingRewards}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                              setProjectSettingStakingRewards(parseFloat(e?.target?.value));
+                            }}
+                          />
                           <div className="absolute right-5 top-3.5 text-xs md:top-3 md:text-sm">
                             <label>Rewards Percentage (max 10%)</label>
                           </div>
@@ -256,7 +312,13 @@ const Index = () => {
                     <div className="text-lg font-bold text-black">
                       <div className="field">
                         <div className="relative">
-                          <input type="number" step="any" max="10" min="0" name="percentage" placeholder="5%" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500    disabled:text-gray-500" value="5" />
+                          <input
+                            type="number" step="any" max="10" min="0" name="percentage" placeholder="5%" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500    disabled:text-gray-500"
+                            value={projectSettingPenalty}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                              setProjectSettingPenalty(parseFloat(e?.target?.value));
+                            }}
+                          />
                           <div className="absolute right-5 top-3.5 text-xs md:top-3 md:text-sm">
                             <label>Penalty Percentage (max 10%)</label>
                           </div>
@@ -272,7 +334,13 @@ const Index = () => {
                     <div className="text-lg font-bold text-black">
                       <div className="field">
                         <div className="relative">
-                          <input type="number" step="any" max="365" min="0" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500    disabled:text-gray-500" value="365" />
+                          <input
+                            type="number" step="any" max="365" min="0" className="w-full p-2 bg-white border border-slate-300 rounded-md shadow-md placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500    disabled:text-gray-500"
+                            value={projectSettingDaysPenalty}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                              setProjectSettingDaysPenalty(parseInt(e?.target?.value));
+                            }}
+                          />
                           <div className="absolute right-5 top-3.5 text-xs md:top-3 md:text-sm">
                             <label>(max 365 days)</label>
                           </div>
@@ -284,7 +352,10 @@ const Index = () => {
               </div>
             </div>
             <div className="w-full md:ml-4 md:w-1/3">
-              <UploadImage />
+              <UploadImage
+                artworkURL={artworkURL}
+                setArtworkURL={setArtworkURL}
+              />
             </div>
           </div>
           {activeChain?.unsupported ? '' : (
